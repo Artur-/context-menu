336406b1828629554c93f926ee2838109901d5cd
Artur Signell <artur@vaadin.com>
2016-09-16 15:33:19 +0300

    Add travis

--- .travis.after_success.sh
+++ .travis.after_success.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo "SONATYPE_USERNAME: $SONATYPE_USERNAME"
-echo "FOO: $FOO"
-
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_SECURE_ENV_VARS" = "true" ]
 then
 	cd vaadin-context-menu-addon
