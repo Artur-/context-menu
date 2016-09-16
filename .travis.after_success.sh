bd3564493a94aec2bc9a8b966f40461159d40627
Artur Signell <artur@vaadin.com>
2016-09-16 15:09:21 +0300

    Add travis

--- .travis.after_success.sh
+++ .travis.after_success.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$TRAVIS_BRANCH" = "master" ]
+if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_SECURE_ENV_VARS" = "true"]
 then
 	cd vaadin-context-menu-addon
 	mvn deploy --settings ../.travis-settings.xml -DrepositoryId=vaadin-snapshots -DaltDeploymentRepository=vaadin-snapshots::default::https://oss.sonatype.org/content/repositories/vaadin-snapshots/ -DskipTests=true -DskipITs -B
